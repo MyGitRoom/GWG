40f9838ec744abf70a485e36f7e9c569f97772fa
lanou <lan@lanoudeMac-mini-174.local>
2016-05-11 19:42:45 +0800

    zf

--- GWG_Project/MovieViewController.m
+++ GWG_Project/MovieViewController.m
@@ -12,10 +12,14 @@
 #import  "NetWorkRequestManager.h"
 #import "MovieModel.h"
 #include "TypeOfMovieViewController.h"
-@interface MovieViewController ()<UICollectionViewDelegate,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource>
+#import "XRCarouselView.h"
+@interface MovieViewController ()<UICollectionViewDelegate,UICollectionViewDelegateFlowLayout,UICollectionViewDataSource,UIScrollViewDelegate ,XRCarouselViewDelegate>
 
 @property (nonatomic ,strong)UICollectionView *collectView ;
 @property (nonatomic ,strong)NSMutableArray<MovieModel*>  *array ;
+
+@property (nonatomic ,strong) XRCarouselView *scrollView ;
+
 @end
 
 @implementation MovieViewController
@@ -35,6 +39,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     
     self.navigationController.navigationBarHidden = NO ;
     [self getData];
@@ -47,6 +52,7 @@
     
     
 }
+
 
 
 //获取数据
@@ -99,9 +105,9 @@
     //边沿距离
     flowLayout.sectionInset  = UIEdgeInsetsMake(2, 10, 10, 10);
     
-    
     self.collectView = [[UICollectionView alloc]initWithFrame:CGRectMake(0,0, KScreenWidth, KScreenHeight) collectionViewLayout:flowLayout];
-    self.collectView.backgroundColor = [UIColor colorWithWhite:0.908 alpha:1.000];
+//    self.collectView.backgroundColor = [UIColor colorWithWhite:0.908 alpha:1.000];
+        self.collectView.backgroundColor = [UIColor colorWithRed:0.024 green:0.031 blue:0.063 alpha:1.000];
     
     self.collectView.delegate = self ;
     self.collectView.dataSource = self ;
@@ -110,25 +116,71 @@
 
     [self.collectView registerClass:[MovieCollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
     
+    //注册分区头
+    [self.collectView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"header"];
+    
     [self.view addSubview:self.collectView];
- 
-
 
 }
 
 -(NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
-
-//    NSInteger i = self.array.count - 14 ;
     return  self.array.count  ;
-
 }
+
+
 
 
 -(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
-//   NSLog(@"%f,%f",(KScreenWidth-40)/3,((KScreenWidth-30)/3+20));
+
     return CGSizeMake((KScreenWidth-45)/3,(KScreenWidth-45)/3/0.92);
-    
+
 }
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section{
+
+    return CGSizeMake(KScreenWidth, KScreenHeight/3) ;
+}
+
+//设置collectionView 头
+-(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath{
+
+    UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"header" forIndexPath:indexPath];
+    
+    [[headerView.subviews lastObject] removeFromSuperview] ;
+    
+    //网络图片
+    NSArray *arr2 = @[@"http://pic6.qiyipic.com/image/20150511/1d/b9/v_109142000_m_601_480_270.jpg", @"http://img32.mtime.cn/up/2013/03/10/235922.90710072_o.jpg", @"http://photocdn.sohu.com/20090519/Img264054148.jpg"];
+    
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, 0, KScreenWidth, KScreenHeight/3)];
+    
+    self.scrollView = [XRCarouselView carouselViewWithImageArray:arr2];
+    self.scrollView.frame = CGRectMake(0, 0, KScreenWidth, KScreenHeight/3-40);
+    self.scrollView.time = 2 ;
+    self.scrollView.delegate = self ;
+    
+    //设置分页控件的frame
+    CGFloat width = arr2.count * 30;
+    CGFloat height = 20;
+    CGFloat x = (_scrollView.frame.size.width - width )/2;
+    CGFloat y = _scrollView.frame.size.height - height - 3;
+    _scrollView.pageControl.frame = CGRectMake(x, y, width, height);
+//    _scrollView.pageControl.currentPageIndicatorTintColor = [UIColor blackColor];
+    _scrollView.pageControl.pageIndicatorTintColor = [UIColor blackColor];
+    
+  UILabel *titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(10,KScreenHeight/3-35,KScreenWidth , 30)];
+        titleLabel.text = @"电影类型" ;
+//    titleLabel.backgroundColor = [UIColor yellowColor];
+        titleLabel.textColor = [UIColor whiteColor ];
+        titleLabel.font = [UIFont systemFontOfSize:18];
+    
+        [view addSubview:self.scrollView];
+        [view addSubview:titleLabel];
+        [headerView addSubview:view];
+    
+    return headerView ;
+}
+
+
 
 -(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
 
@@ -139,6 +191,7 @@
 
     [cell.imageView sd_setImageWithURL:[NSURL URLWithString:self.array[indexPath.item].img_url]];
     return cell ;
+
 }
 
 //点击item进行跳转
@@ -155,19 +208,7 @@
    
     
     
-    /*
-//    [self.navigationController pushViewController:typeMovie animated:YES];
-//    NSLog(@"%@",self.array[indexPath.item].identifier);
-//    [NetWorkRequestManager requestWithType:POST urlString:@"http://114.215.104.21/v130/singles/catlist" ParDic:@{@"Content-Type":@"application/x-www-form-urlencoded",@"start":@"0",@"count":@"15",@"id":self.array[indexPath.item].identifier} finish:^(NSData *data) {
-//        
-//        NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-//        NSLog(@"%@",[dic objectForKey:@"data"]);
-//    } err:^(NSError *error) {
-//        
-//    }];
-//  
-    
-*/
+
 
 }
 
